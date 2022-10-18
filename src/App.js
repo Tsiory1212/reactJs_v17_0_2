9161b58ec3839b459eedf5bbe28b10fa18bd91cd
Tsiory <fordevproject@gmail.com>
2022-10-18 14:18:10 +0200

    HOC : Comprendre la syntaxe

--- src/App.js
+++ src/App.js
@@ -1,58 +1,21 @@
 import './App.css';
-import { useRef, useEffect, useContext } from 'react';
-import {Context} from "./Context";
 
-function Form() {
-   const inputRef = useRef();
-   const {onChange, inputValue, addItem} = useContext(Context);
-
-   const handleOnSubmit = (e) => {
-      e.preventDefault();
-      if (!inputValue) {
-         return false;
-      }
-      addItem();
-      inputRef.current.value = null;
-   }
-
-
-
-   return (
-      <>
-         <form onSubmit={handleOnSubmit}>
-            <input ref={inputRef} type="text" onChange={onChange} /> <button type="submit">Add</button>
-         </form>
-         <br/>
-      </>
-
-   );
+const withHOC = (Component) => () => {
+   const learn = "Learn React";
+   return <Component text={learn}/>
 }
 
-function Todos() {
-   const {state, removeItem} = useContext(Context);
-
-
-   return(
-      <ul>
-         {state.items.map((item, index) => {
-            return <li key={index} onClick={() => removeItem(item.id)}>{item.text}</li>
-         })}
-      </ul>
-   );
-}
-
-function App() {
+function App({text}) {
    // useEffect(() => console.log(state.value), [state.value]);
 
    return (
       <div className="App">
          <header className="App-header">
-            <Form/>
-            <Todos />
+         {text}
          </header>
       </div>
    );
 }
 
 
-export default App;
+export default withHOC(App);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Provider from './Context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
     <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 
